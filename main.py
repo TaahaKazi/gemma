5db4cb9c300129d6cb4f1b26d6f87b77bc57ed1b
Taaha Kazi <taahakazi@gmail.com>
2024-04-13 13:24:02 -0500

    adding sql prompts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+
 os.environ['HF_TOKEN'] = json.loads(open('key.json').read())['HF_TOKEN']
 
 import torch
@@ -11,11 +12,14 @@ from datasets import load_dataset
 
 
 def formatting_func(example):
-    text = f"Quote: {example['quote'][0]}\nAuthor: {example['author'][0]}"
-    return [text]
+    output_texts = []
+    for i in range(len(example['sql_prompt'])):
+        text = f"Context: {example['sql_context'][i]}\nTask: {example['sql'][i]}\nSolution:\nExplanation: {example['sql_explanation'][i]}\nAnswer: {example['sql'][i]}]"
+        output_texts.append(text)
+    return output_texts
+
 
 def run(model_id="google/gemma-7b"):
-
     model_id = "google/gemma-7b"
 
     bnb_config = BitsAndBytesConfig(
@@ -25,15 +29,8 @@ def run(model_id="google/gemma-7b"):
     )
 
     tokenizer = AutoTokenizer.from_pretrained(model_id, token=os.environ['HF_TOKEN'])
-    model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map={"":0}, token=os.environ['HF_TOKEN'])
-
-    # Generate sample text
-    text = "Quote: Imagination is more"
-    device = "cuda:0"
-    inputs = tokenizer(text, return_tensors="pt").to(device)
-
-    outputs = model.generate(**inputs, max_new_tokens=20)
-    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+    model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map={"": 0},
+                                                 token=os.environ['HF_TOKEN'], attn_implementation="flash_attention_2")
 
     # Save the model
     os.environ["WANDB_DISABLED"] = "true"
@@ -44,36 +41,43 @@ def run(model_id="google/gemma-7b"):
         task_type="CAUSAL_LM",
     )
 
-    data = load_dataset("Abirate/english_quotes")
-    data = data.map(lambda samples: tokenizer(samples["quote"]), batched=True)
+    data = load_dataset("gretelai/synthetic_text_to_sql")
+    # TODO: Add data collator
+
 
     trainer = SFTTrainer(
         model=model,
-        train_dataset=data["train"],
+        train_dataset=data["test"],
+        max_seq_length=1024,
         args=transformers.TrainingArguments(
             per_device_train_batch_size=1,
             gradient_accumulation_steps=4,
-            warmup_steps=2,
-            max_steps=10,
+            warmup_steps=100,
+            #num_train_epochs=1,
+            max_steps=5,
             learning_rate=2e-4,
             fp16=True,
-            logging_steps=1,
+            logging_steps=10,
             output_dir="outputs",
-            optim="paged_adamw_8bit"
+            optim="paged_adamw_8bit",
         ),
         peft_config=lora_config,
         formatting_func=formatting_func,
     )
     trainer.train()
 
-
-    text = "Quote: Imagination is"
+    # Sample Test
+    example = data["train"][0]
+    text = f"Context: {example['sql_context']}\nTask: {example['sql']}\nSolution:\nExplanation: "
     device = "cuda:0"
     inputs = tokenizer(text, return_tensors="pt").to(device)
 
-    outputs = model.generate(**inputs, max_new_tokens=20)
+    outputs = model.generate(**inputs, max_new_tokens=200)
     print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
+    # Save the model
+    model.save_pretrained("outputs")
+    tokenizer.save_pretrained("outputs")
 
 if __name__ == "__main__":
     run()
